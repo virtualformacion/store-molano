a54ae9358b798adb570ad8becacc60d28dd220eb
virtualformacion <virtualformacion47@gmail.com>
2025-03-24 15:01:54 +0000

    getLastEmail.js

--- netlify/functions/getLastEmail.js
+++ netlify/functions/getLastEmail.js
@@ -89,6 +89,7 @@ exports.handler = async (event) => {
       "Importante: Cómo actualizar tu Hogar con Netflix",
       "Importante: Cómo cambiar tu Hogar con Netflix",
       "Tu código de acceso temporal de Netflix",
+      "Completa tu solicitud de cambio de contraseña",
       "Completa tu solicitud de restablecimiento de contraseña"
     ];
 
