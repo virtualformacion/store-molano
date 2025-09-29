135d7cc7d385a9d0656ba003b2ea2982f887f960
virtualformacion <virtualformacion47@gmail.com>
2025-09-29 11:33:44 -0500

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ========== USUARIOS AUTORIZADOS ==========
 const USERS = [
     { username: "admin", password: "1234", expiresAt: new Date("2025-11-30") },
-    { username: "joel", password: "1234567", expiresAt: new Date("2025-09-28") },
+    { username: "joel", password: "1234567", expiresAt: new Date("2025-10-29") },
     { username: "luwe", password: "stream", expiresAt: new Date("2025-09-29") }
 ];
 
