d572a69255ac3ec9f3815921ee7ed46e2e1c02c4
virtualformacion <virtualformacion47@gmail.com>
2026-06-07 16:41:33 -0500

    getLastEmail.js

--- netlify/functions/getLastEmail.js
+++ netlify/functions/getLastEmail.js
@@ -47,6 +47,7 @@ exports.handler = async (event) => {
       "amazon.com: Intento de inicio de sesión",
       "Este código vence en 15 minutos",
       "Código de verificación. Caduca en 15 min",
+      "Código de verificación. Caduca en 15 min",
       "Netflix: Tu código de inicio de sesión",
       "Netflix: Tu código de inicio de sesión",
       "Your one-time passcode for Disney+", 
