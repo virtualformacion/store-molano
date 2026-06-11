f1b02389c5903039f1c5f950ccdb0ba0646af869
virtualformacion <virtualformacion47@gmail.com>
2026-06-11 18:00:56 -0500

    getLastEmail.js

--- netlify/functions/getLastEmail.js
+++ netlify/functions/getLastEmail.js
@@ -30,9 +30,8 @@ exports.handler = async (event) => {
     // Pausa aleatoria antes de realizar la búsqueda de correos
     await delay();
 
-    const response = await gmail.users.messages.list({
-      userId: "me",
-      maxResults: 60, // Buscar hasta 10 correos
+  
+      maxResults: 110, // Buscar hasta 10 correos
     });
 
     console.log("📩 Correos encontrados:", response.data.messages);
@@ -44,12 +43,17 @@ exports.handler = async (event) => {
     // ------------ Lógica de Disney+ -----------------
     const disneySubjects = [
       "amazon.com: Sign-in attempt",
+      
       "amazon.com: Intento de inicio de sesión",
+      
       "Este código vence en 15 minutos",
+      
       "Netflix: Tu código de inicio de sesión",
-      "Netflix: Tu código de inicio de sesión",
-      "Your one-time passcode for Disney+", 
+      
+      "Your one-time passcode for Disney+",
+      
       "Tu clave de un solo uso para Disney+",
+      
       "Tu código de acceso único para Disney+" // Asunto específico de Disney+
     ];
 
@@ -91,10 +95,15 @@ exports.handler = async (event) => {
     // ------------ Lógica de Netflix -----------------
     const validSubjects = [
       "Importante: Cómo actualizar tu Hogar con Netflix",
+      
       "Importante: Cómo cambiar tu Hogar con Netflix",
-      "Tu código de acceso temporal de Netflix", 
+      
       "Tu código de acceso temporal de Netflix",
+  
+      "Tu código de acceso temporal de Netflix",
+      
       "Completa tu solicitud de cambio de contraseña",
+      
       "Completa tu solicitud de restablecimiento de contraseña"
     ];
 
